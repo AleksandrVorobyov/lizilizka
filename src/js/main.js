e312e0eb903294cbbab12484c48b7270a7d26933
Alex <sasha89253215539@yandex.ru>
2020-10-07 16:26:40 +0300

    правка шапки

--- src/js/main.js
+++ src/js/main.js
@@ -1,66 +1,67 @@
-$(document).ready(function(){
-    $(".owl-carousel-header").owlCarousel({
-        items: 3,
-        loop: false,
-        nav: false,
-        dots: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            1440: {
-                items: 2,
-            },
-            2170: {
-                items: 3
-            }
-        },
-        stagePadding: 0,
-        arrows: false,
-        margin: 10,
-        mouseDrag : false,
-        touchDrag : false
-    });
-  });
-
-$(document).ready(function(){
-    $(".owl-carousel-brow").owlCarousel({
+$(document).ready(function () {
+  $(".owl-carousel-header").owlCarousel({
+    items: 3,
+    loop: false,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
         items: 1,
-        loop: false,
-        nav: true,
-        navText: ["Назад", "Вперед"],
-        dots: false,
-        stagePadding: 0,
-        arrows: false,
-        margin: 10,
-        autoWidth: false
-    });
-  });
-
-$(document).ready(function(){
-    $(".owl-carousel-milesnick").owlCarousel({
-        items: 1,
-        loop: false,
-        nav: true,
-        navText: ["Назад", "Вперед"],
-        dots: false,
-        stagePadding: 0,
-        arrows: false,
-        margin: 10,
-        // autoWidth: true
-    });
-  });
-
-$(document).ready(function(){
-    $(".owl-carousel-lips").owlCarousel({
+      },
+      1440: {
         items: 2,
-        loop: false,
-        nav: true,
-        navText: ["Назад", "Вперед"],
-        dots: false,
-        stagePadding: 0,
-        arrows: false,
-        margin: 10,
-        autoWidth: false
-    });
+      },
+      2170: {
+        items: 3,
+      },
+    },
+    stagePadding: 0,
+    arrows: false,
+    margin: 10,
+    mouseDrag: false,
+    touchDrag: false,
   });
+});
+
+$(document).ready(function () {
+  $(".owl-carousel-brow").owlCarousel({
+    items: 1,
+    loop: false,
+    nav: true,
+    navText: ["Назад", "Вперед"],
+    dots: false,
+    stagePadding: 0,
+    arrows: false,
+    margin: 10,
+    autoWidth: false,
+  });
+});
+
+$(document).ready(function () {
+  $(".owl-carousel-milesnick").owlCarousel({
+    items: 1,
+    loop: false,
+    nav: true,
+    navText: ["Назад", "Вперед"],
+    dots: false,
+    stagePadding: 0,
+    arrows: false,
+    margin: 10,
+  });
+});
+
+$(document).ready(function () {
+  $(".owl-carousel-lips").owlCarousel({
+    items: 2,
+    loop: false,
+    nav: true,
+    navText: ["Назад", "Вперед"],
+    dots: false,
+    stagePadding: 0,
+    arrows: false,
+    margin: 10,
+    autoWidth: false,
+  });
+});
+
+
