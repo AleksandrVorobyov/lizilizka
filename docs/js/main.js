52952ab434811d7b3fac9f827379c8f54b1c4480
Alex <sasha89253215539@yandex.ru>
2020-11-03 19:14:17 +0300

    добавил js

---
+++ docs/js/main.js
@@ -0,0 +1,112 @@
+$(document).ready(function () {
+  $(".owl-carousel-header").owlCarousel({
+    items: 3,
+    loop: false,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+        slideBy: 2
+      },
+      1440: {
+        items: 2,
+      },
+      2170: {
+        items: 3,
+      },
+    },
+    stagePadding: 0,
+    arrows: false,
+    margin: 10,
+    mouseDrag: false,
+    touchDrag: false,
+  });
+});
+
+$(document).ready(function () {
+  $(".owl-carousel-brow").owlCarousel({
+    items: 1,
+    loop: false,
+    nav: true,
+    navText: ["Назад", "Вперед"],
+    dots: false,
+    stagePadding: 0,
+    arrows: false,
+    margin: 10,
+    autoWidth: false,
+  });
+});
+
+$(document).ready(function () {
+  $(".owl-carousel-milesnick").owlCarousel({
+    items: 1,
+    loop: false,
+    nav: true,
+    navText: ["Назад", "Вперед"],
+    dots: false,
+    stagePadding: 0,
+    arrows: false,
+    margin: 10,
+    autoWidth:true,
+    autoHeight:true
+  });
+});
+
+$(document).ready(function () {
+  $(".owl-carousel-lips").owlCarousel({
+    items: 2,
+    loop: false,
+    nav: true,
+    navText: ["Назад", "Вперед"],
+    dots: false,
+    stagePadding: 0,
+    arrows: false,
+    margin: 10,
+    autoWidth: false,
+  });
+});
+
+var formButton = document.querySelectorAll("[data-form]");
+var formPosht = document.querySelector("#form");
+var formExit = document.querySelectorAll("[data-exit]");
+var formSubmit = document.querySelectorAll("[data-submit]");
+var success = document.querySelector('.form-success');
+var body = document.querySelector('body');
+
+formButton.forEach(function (e) {
+  e.addEventListener("click", function (e) {
+    function unlockForm() {
+      formPosht.style.display = "block";
+      body.classList.add('lock');
+    }
+
+    unlockForm();
+  });
+});
+
+formExit.forEach(function (e) {
+  e.addEventListener("click", function (e) {
+    function closeForm() {
+      formPosht.style.display = "none";
+      body.classList.remove('lock');
+    }
+
+    closeForm();
+  });
+});
+
+formSubmit.forEach(function (e) {
+  e.addEventListener("click", function (e) {
+    function closeForm() {
+      formPosht.style.display = "none";
+      success.classList.add('form-success--active');
+      setTimeout(() => success.classList.remove('form-success--active'), 5000);
+      body.classList.remove('lock');
+    }
+
+    closeForm();
+  });
+});
+
+
