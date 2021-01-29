e5711bba809dae3d17101a332a446ec4b977ee33
Alex <sasha89253215539@yandex.ru>
2021-01-29 14:44:28 +0300

    mobile first ready

--- src/js/main.js
+++ src/js/main.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  $(".owl-carousel-brow").owlCarousel({
+  $(".brow__carousel").owlCarousel({
     items: 1,
     loop: false,
     nav: true,
@@ -39,7 +39,7 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  $(".owl-carousel-milesnick").owlCarousel({
+  $(".milesnick__carousel").owlCarousel({
     items: 1,
     loop: false,
     nav: true,
@@ -48,14 +48,12 @@ $(document).ready(function () {
     stagePadding: 0,
     arrows: false,
     margin: 10,
-    autoWidth:true,
-    autoHeight:true
+    autoWidth: false,
   });
 });
 
 $(document).ready(function () {
-  $(".owl-carousel-lips").owlCarousel({
-
+  $(".lips__carousel").owlCarousel({
     loop: false,
     nav: true,
     navText: ["Назад", "Вперед"],
